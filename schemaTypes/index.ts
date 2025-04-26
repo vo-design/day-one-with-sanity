7e3c7d22331cee5ef8f196a00d6142a569fb19f1
Volodymyr Onishchenko <hello@vdigital.design>
2025-04-26 15:50:31 +0300

    Creating a schema

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,1 +1,5 @@
-export const schemaTypes = []
+import {eventType} from './eventType'
+import {artistType} from './artistType'
+import {venueType} from './venueType'
+
+export const schemaTypes = [artistType, eventType, venueType]
